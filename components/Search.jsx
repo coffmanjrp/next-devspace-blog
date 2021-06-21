88c7798cd4d779ba54862583622449152fa20acc
coffmanjrp <coffmanjrp@gmail.com>
2021-06-20 18:38:21 -0700

    Implement search api route

--- components/Search.jsx
+++ components/Search.jsx
@@ -3,7 +3,22 @@ import { FaSearch } from 'react-icons/fa';
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const getResults = async () => {
+      if (searchTerm === '') {
+        setSearchResults([]);
+      } else {
+        const res = await fetch(`/api/search?q=${searchTerm}`);
+        const { results } = await res.json();
+        console.log(results);
+        setSearchResults(results);
+      }
+    };
+
+    getResults();
+  }, [searchTerm]);
 
   return (
     <div className="relative bg-gray-600 p-4">
