21db61c8552420baff04c93764f24e0b46cdf481
coffmanjrp <coffmanjrp@gmail.com>
2021-06-20 18:17:23 -0700

    Create Search component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Header from './Header';
+import { Header, Search } from '.';
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -12,6 +12,7 @@ export default function Layout({ title, keywords, description, children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Search />
       <main className="container mx-auto my-7">{children}</main>
     </div>
   );
--- components/index.js
+++ components/index.js
@@ -4,3 +4,4 @@ export { default as Header } from './Header';
 export { default as Layout } from './Layout';
 export { default as Pagination } from './Pagination';
 export { default as Post } from './Post';
+export { default as Search } from './Search';
